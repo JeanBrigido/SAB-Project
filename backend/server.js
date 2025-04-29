fc73860083b9f75817ffafb96cfbf2e52025388c
JeanBrigido <jeanj.brigido@gmail.com>
2025-04-28 23:15:54 -0500

    Backend

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import verseRoutes from './routes/verse.js';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // CORS configuration
 const corsOptions = {
