ab0078ba64ef381a6952fc71faf188b4f405f6a5
JeanBrigido <jeanj.brigido@gmail.com>
2025-04-29 11:39:03 -0500

    URL Re

--- frontend/src/config/asgardeo-config.js
+++ frontend/src/config/asgardeo-config.js
@@ -1,6 +1,6 @@
 export const config = {
-    signInRedirectURL: "http://localhost:5173/",
-    signOutRedirectURL: "http://localhost:5173/",
+    signInRedirectURL: "https://sab-project-8pv7.vercel.app/",
+    signOutRedirectURL: "https://sab-project-8pv7.vercel.app/",
     clientID: "wUKMSl0DpGFLPQJyEMoTx5CfdG4a",
     baseUrl: "https://api.asgardeo.io/t/fullstack372",
     scope: [ "openid","profile" ]
